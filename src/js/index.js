a9d88a8f7b535f3722b0e5da73b2185a99f624d4
Vladimir <shadrov_vladimir@mail.ru>
2022-09-26 20:22:23 +0300

    feat: Старт проекта

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import '../styles/styles.scss';
+
+function getData() {
+  let response = fetch('https://fortniteapi.io/v2/items/list?lang=ru', { method: 'GET', headers: { Authorization: '24d13aee-b446a7e4-bbdedcee-62e62bf4' } })
+    .then((response) => response.json())
+    .then((response) => {
+      // const res = response.items.filter((item) => item.name.indexOf('Ара') > 0); // Фильтрация по имени
+      // const res = response.items.filter((item) => item.set !== null);
+      // const res = response.items.map((item) => item.set !== null && item.set.name); // Фильтрация по названию набора
+      const types = response.items.map((item) => item.type.name); // Фильтрация по типу
+      const res = response.items.filter((item) => item.type.name.toLowerCase() === 'Экипировка'.toLowerCase()); // Фильтрация по экипировка
+      console.log('Тип героя: ', new Set(types));
+      console.log('Массив по типам: ', res);
+    });
+}
+
+// getData();
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: '../src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/styles.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          devMode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[path]/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[path]/[name][ext]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    port: 2020,
+    open: true,
+    hot: false,
+    client: {
+      logging: 'none',
+    },
+  },
+};
